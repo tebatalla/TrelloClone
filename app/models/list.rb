3ca3d4507b31be78c7e4fe48b723f655ae5707ea
Tomas Batalla <tebatalla@gmail.com>
2015-04-03 15:53:19 -0700

    Method to increment on max list ord for that board when adding a new list

--- app/models/list.rb
+++ app/models/list.rb
@@ -12,11 +12,23 @@
 
 class List < ActiveRecord::Base
   validates :title, :board, :ord, presence: true
+  after_initialize :increment_max_ord
 
   belongs_to :board
   has_many :cards, dependent: :destroy
 
   default_scope { order(:ord) }
+
+  def increment_max_ord
+    if ord == 0.0
+      ord_max = List.where(board_id: board.id).maximum(:ord)
+      if ord_max.nil?
+        self.ord = 1.0
+      else
+        self.ord = ord_max + 1
+      end
+    end
+  end
 
   # TODO: class method for updating orders?
 end

adccdf86fe3a0b60351253fdbfa4d34e07cf8ce0
Tomas Batalla <tebatalla@gmail.com>
2015-04-06 16:56:39 -0700

    Persist list changes, prevent new list form from being sorted

--- app/assets/javascripts/views/board/board_show.js
+++ app/assets/javascripts/views/board/board_show.js
@@ -25,6 +25,44 @@ TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
 
   },
 
+  dropDraggable: function (event, ui) {
+    var lists = ui.item.parent().children();
+    var newListModels = [];
+    _.each( lists.slice(0, lists.length - 1), function (list, index) {
+      var listId = $(list).data('id');
+      var listModel = this.model.lists().get(listId);
+      listModel.set('ord', index);
+      listModel.save({});
+      newListModels.push(listModel);
+    }.bind(this));
+
+    this.model.lists().set(newListModels);
+    // var cards = ui.item.parent().children();
+
+    // _.each( cards, function (card, index) {
+    //   var cardModel, oldCollection;
+    //   var newCollection = this.collection.get(newListId).cards();
+    //   var id = $(card).data('id');
+    //   if ($(card).data('id') === ui.item.data('id')) {
+    //     oldCollection = this.model.cards();
+    //     cardModel = oldCollection.get(id);
+    //   } else {
+    //     oldCollection = newCollection;
+    //     cardModel = oldCollection.get(id);
+    //   }
+    //   cardModel.set({
+    //     "ord": index,
+    //     "list_id": newListId
+    //   });
+    //   cardModel.save({}, {
+    //     success: function (cardModel) {
+    //       oldCollection.remove(cardModel);
+    //       newCollection.add(cardModel);
+    //     }.bind(this)
+    //   });
+    // }.bind(this));
+  },
+
   render: function () {
     var content = this.template({
       board: this.model
@@ -41,18 +79,17 @@ TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
       this.addSubview('.lists', listView);
     }.bind(this));
 
-    this.$newList = $('<li class="list-item">');
+    this.$newList = $('<li class="list-item not-sortable">');
     this.$newList.append('<div class="list">');
     this.$newList.find('.list').append('<a class="new-list">Add a new list</a>');
     this.$('.lists').append(this.$newList);
 
 
-    // this.$el.append(newListView.render().$el);
-
-
     this.$('.lists').sortable({
       connectWith: '.lists',
-      placeholder: "list-placeholder"
+      placeholder: "list-placeholder",
+      stop: this.dropDraggable.bind(this),
+      items: "li:not(.not-sortable)"
     });
   
     return this;
